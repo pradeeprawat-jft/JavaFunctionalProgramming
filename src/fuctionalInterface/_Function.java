9bfa920b709d5e4d8bebba655b872d9949e36278
pradeep rawat <pradeep.rawat@jellyfishtechnologies.com>
2023-06-01 13:31:03 +0530

    added functional chaining funcion

--- src/fuctionalInterface/_Function.java
+++ src/fuctionalInterface/_Function.java
@@ -4,13 +4,25 @@ import java.util.function.Function;
 
 public class _Function {
 
+    static Function<Integer, Integer> incrementByOneFunction = number -> ++number;
+    static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
+
+    //    FunctionalInterface function chaining method
+    static Function<Integer, Integer> addByOneAndMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10);
+
     public static void main(String[] args) {
         Integer number = incrementByOne(1);
         System.out.println(number);
+
+        Integer number2 = incrementByOneFunction.apply(1);
+        System.out.println(number2);
+        
+        //function Chaining
+        Integer ans = addByOneAndMultiplyBy10.apply(10);
+        System.out.println(ans);
     }
 
-    static Function<Integer,Integer>
     public static Integer incrementByOne(Integer number) {
-        return number++;
+        return number + 1;
     }
 }
