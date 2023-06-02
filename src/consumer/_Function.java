a2f52f23d1c8f66e794dda54094be76212ecd4fb
pradeep rawat <pradeep.rawat@jellyfishtechnologies.com>
2023-06-02 17:22:01 +0530

    Consumer Added

---
+++ src/consumer/_Function.java
@@ -0,0 +1,29 @@
+package consumer;
+
+import java.util.function.Consumer;
+
+public class _Function {
+    static Consumer<Customer> getConsumer = customer -> System.out.println("Hello " + customer.name + ". Registered Mobile number is " + customer.mobile);
+
+    public static void main(String[] args) {
+        String greet = greeting(new Customer("pradeep", "09876543211"));
+        System.out.println(greet);
+        getConsumer.accept(new Customer("pradeep", "09876543211"));
+
+
+    }
+
+    public static String greeting(Customer customer) {
+        return "Hello " + customer.name + ". Registered Mobile number is " + customer.mobile;
+    }
+
+    static class Customer {
+        private final String name;
+        private final String mobile;
+
+        Customer(String name, String mobile) {
+            this.name = name;
+            this.mobile = mobile;
+        }
+    }
+}
